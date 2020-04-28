772179ac9c8e9a026fdba16481f8d5b004e8e590
Marco Henrique Nunes Pereira <marcohnp@hotmail.com>
2020-04-28 14:37:21 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -3,9 +3,9 @@ package model.entities;
 import java.io.Serializable;
 
 public class Department implements Serializable {
-
+	
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	private String name;
 	
@@ -60,6 +60,7 @@ public class Department implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
+		return "Departament [id=" + id + ", name=" + name + "]";
 	}
+	
 }
